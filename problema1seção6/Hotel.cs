164578548cc064e103c4450adbeef39593f2aad0
Pedro Guimaraes <pedromonteirogui@gmail.com>
2020-05-09 22:08:51 -0300

    Exercicio de fixação vetor

---
+++ problema1seção6/Hotel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace problema1seção6
+{
+    class Hotel
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+
+        public int Quarto { get; set; }
+        public Hotel(string nome, string email, int quarto)
+        {
+            Nome = nome;
+            Email = email;
+            Quarto = quarto;
+        }
+
+        public override string ToString()
+        {
+            return Quarto + ": " + Nome + ", " + Email;
+        }
+    }
+}
---
+++ problema1seção6/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+using System.Runtime.CompilerServices;
+
+namespace problema1seção6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe quantos quartos serão alugados");
+            int quartos = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Hotel[] hotel = new Hotel[quartos];
+
+            for (int i = 0; i < hotel.Length; i++)
+            {
+                Console.Write("Informe o nome: ");
+                string nome = Console.ReadLine();
+                Console.Write("Informe o e-mail: ");
+                string email = Console.ReadLine();
+                Console.Write("Informe o quarto desejado (0 a 9): ");
+                int quarto = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.WriteLine();
+
+                hotel[i] = new Hotel(nome, email, quarto);
+            }
+
+            Console.WriteLine("Quartos alugados:");
+            for (int i = 0; i < hotel.Length; i++)
+            {
+                Console.WriteLine(hotel[i]);
+            }
+        }
+    }
+}
